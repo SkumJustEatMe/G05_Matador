b4ef7ac8acb43b80fbe0d6fd3d0ec416fc422e72
Emilkroldrup <113909369+Emilkroldrup@users.noreply.github.com>
2023-01-09 19:44:03 +0100

    Create JailRules.java

---
+++ src/main/java/game/JailRules.java
@@ -0,0 +1,22 @@
+package game;
+
+
+public class JailRules {
+
+
+    //Funktion til at -1000 balance og derefter giver samme player turen igen.
+public static void PayOutOfJail(Player player){
+
+    player.changeBalance(-1000);
+
+}
+
+
+
+
+
+
+
+
+
+}
