b3d4fc64a1b8af940cd61b5322ff71ee3902c55a
Emilkroldrup <113909369+Emilkroldrup@users.noreply.github.com>
2023-01-09 23:42:33 +0100

    Update JailRules.java

--- src/main/java/game/JailRules.java
+++ src/main/java/game/JailRules.java
@@ -4,18 +4,26 @@ package game;
 public class JailRules {
 
 
-    //Funktion til at -1000 balance og derefter giver samme player turen igen.
+    //Function that takes -1000 from the player balance and let them roll again
 public static void PayOutOfJail(Player player){
 
     player.changeBalance(-1000);
 
 }
 
-public String ExtraTurn(return ThrowDicesAgain){
-    
+
+
+//Function to give an extra turn if you roll equals
+public String ExtraTurn(){
+
+
 }
 
+public static void ForceOutOfJail{
 
+
+    
+    }
 
 
 
